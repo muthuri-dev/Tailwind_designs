27b74370757dfc1d8a84dc8b62635c6aa6614165
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-06-28 11:43:39 +0300

    created the layout of the app

---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='border-2 border-red-500'>Top Navigation</div>
+  )
+}
---
+++ src/components/sideView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideView() {
+  return (
+    <div class>This is the side view</div>
+  )
+}
